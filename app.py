ffe9b2e0ccc0073ff3b262914ac2d5d6a921e6da
KurooAMV <stutisharan752@gmail.com>
2025-05-23 20:25:31 +0530

    More changes

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import os
 import streamlit as st
-import tensorflow as tf
-import keras
 import numpy as np
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from PIL import Image
 
 def preprocess_image(img):
@@ -15,26 +12,27 @@ def preprocess_image(img):
     img = img / 255.0
     return img
 
-model = keras.models.load_model("model/PneumoniaDetectionModel.keras")
+model = load_model("model/PneumoniaDetectionModel.keras")
 
 st.title("Pneumonia Detector Using CNN")
 uploaded_file = st.file_uploader("Upload an Image for Prediction", type=['jpg', 'png', 'jpeg'])
+col1, col2 = st.columns(2)
 
 if uploaded_file is not None:
     image_pil = Image.open(uploaded_file)
     thumbnail = image_pil.copy()
     thumbnail.thumbnail((200, 200)) 
-    st.image(thumbnail, caption="Preview", width=100)
+    col1.image(thumbnail, caption="Preview", width=100)
 
-    if st.button("Predict"):
+    if col1.button("Predict"):
         img_array = preprocess_image(image_pil)
         prediction = model.predict(img_array)
         predicted_class = "Pneumonia" if prediction[0][0] > 0.5 else "Normal"
         confidence = prediction[0][0] if prediction[0][0] > 0.5 else 1 - prediction[0][0]
 
-        st.write("### Predicted Class:")
+        col2.write("### Predicted Class:")
         if predicted_class == "Pneumonia":
-            st.warning("Pneumonia")
+            col2.warning("Pneumonia")
         else:
-            st.success("Normal")
-        st.write(f"### Confidence Level: {confidence:.2%}")
+            col2.success("Normal")
+        col2.write(f"### Confidence Level: {confidence:.2%}")
