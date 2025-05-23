419639c0bad9aeab3b3e993a8d5b533ad37bdd2d
Stuti <101517729+KurooAMV@users.noreply.github.com>
2025-05-23 18:07:04 +0530

    Update and rename new_app.py to app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 import streamlit as st
-import tensorflow as tf
 import keras
 import numpy as np
 from keras.models import load_model
 from PIL import Image
-import gdown
 
 def preprocess_image(img):
     img = img.convert('L')  # Convert to grayscale
@@ -16,35 +14,20 @@ def preprocess_image(img):
     img = img / 255.0
     return img
 
-MODEL_PATH = 'model/pneumonia_detection_model.keras'
-GDRIVE_ID = '1gHoNs4ulIA8HAd29f3uRSMTOTXLcV2MQ1GpHz4Q2JgsdBWGZMNANX4MAgCUtxmUIE'
-GDRIVE_URL = f'https://drive.google.com/uc?id={GDRIVE_ID}'
-
-def download_model():
-    if not os.path.exists(MODEL_PATH):
-        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
-        # print("Downloading model from Google Drive...")
-        gdown.download(GDRIVE_URL, MODEL_PATH, quiet=False)
-    return
+model = keras.models.load_model("model/PneumoniaDetectionModel.keras")
 
 st.title("Pneumonia Detector Using CNN")
 uploaded_file = st.file_uploader("Upload an Image for Prediction", type=['jpg', 'png', 'jpeg'])
 
 if uploaded_file is not None:
     image_pil = Image.open(uploaded_file)
-    # st.image(image_pil, caption="Uploaded X-ray", use_column_width=True)
     thumbnail = image_pil.copy()
     thumbnail.thumbnail((200, 200)) 
     st.image(thumbnail, caption="Preview", width=100)
 
     if st.button("Predict"):
-        # download_model()
-        # model = load_model()
-        download_model()
-        model = keras.models.load_model(MODEL_PATH)
         img_array = preprocess_image(image_pil)
         prediction = model.predict(img_array)
-        os.remove(MODEL_PATH)
         predicted_class = "Pneumonia" if prediction[0][0] > 0.5 else "Normal"
         confidence = prediction[0][0] if prediction[0][0] > 0.5 else 1 - prediction[0][0]
 
