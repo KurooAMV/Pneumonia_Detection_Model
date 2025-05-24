bed21f09a9de3d0aaa2728983588c8976ea9d6a4
KurooAMV <stutisharan752@gmail.com>
2025-05-24 18:29:48 +0530

    unnessecary changed trial1

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if uploaded_file is not None:
 
     if col1.button("Predict"):
         img_array = preprocess_image(image_pil)
-        prediction = model.predict(img_array)
+        prediction = model.predict(None,img_array)
         predicted_class = "Pneumonia" if prediction[0][0] > 0.5 else "Normal"
         confidence = prediction[0][0] if prediction[0][0] > 0.5 else 1 - prediction[0][0]
 
